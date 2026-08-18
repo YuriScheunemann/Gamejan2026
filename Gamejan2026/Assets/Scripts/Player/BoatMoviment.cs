eefa7c7c4a50a41c59b05fd89f0ebd7f516d66a8
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2026-08-18 08:12:42 -0300

    NewProjectSettings

--- Gamejan2026/Assets/Scripts/Player/BoatMoviment.cs
+++ Gamejan2026/Assets/Scripts/Player/BoatMoviment.cs
@@ -3,27 +3,21 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class BoatMoviment : MonoBehaviour
 {
-   private CharacterController charController;
-
-    Rigidbody2D rb;
-    Vector2 movementInput;
+    Rigidbody2D rb;    
     [SerializeField] private float _speed = 2;
-    private float _right;
-    private float _left;
+    private float _moviment;
+
     void Start()
     {
-        charController = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(_left * _speed, _right * _speed);
+        rb.linearVelocity = new Vector2(0, _moviment * _speed);
     }
 
     private void Update()
     {
-        _right = Input.GetAxis("Right");
-        _left = Input.GetAxis("Left");
-       
+        _moviment = Input.GetAxis("Vertical");       
     }
 }
