a04a321a44b46aee0cd99dca4f98a70cb3c29652
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2026-08-19 09:40:32 -0300

    SuitsNoBugsAndWorkingVeryWell
    But its interesting this the suits be more precision

--- Gamejan2026/Assets/Scripts/ObstacleCollision.cs
+++ Gamejan2026/Assets/Scripts/ObstacleCollision.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
--- Gamejan2026/Assets/Scripts/Suits/IfSuitMove.cs
+++ Gamejan2026/Assets/Scripts/Suits/IfSuitMove.cs
@@ -5,15 +5,39 @@ public class IfSuitMove : MonoBehaviour
 {
     [SerializeField] private GameObject[] Suits;
     [SerializeField] private SuitsEnum suitsEnum;
-    [SerializeField] private int _indexSuit; 
+    [SerializeField] private int _indexSuit;
+    private float _lastSuitPosition;
+    void OnMouseDown()
+    {
+        _lastSuitPosition = gameObject.transform.position.y;
+    }
     private void OnMouseDrag()
     {
-        if (gameObject.transform.position.y <= 0)
+        if (gameObject.transform.position.y >= 3)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, 3, gameObject.transform.position.z);
+        }
+        if (gameObject.transform.position.y <= -4)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, -4, gameObject.transform.position.z);
+        }
+        if (gameObject.transform.position.y >= 3 || gameObject.transform.position.y <= -4)
+            return;
+        float newPos = gameObject.transform.position.y;
+        newPos -= _lastSuitPosition;
+        if (newPos == 0)
+            return;
+        StartCoroutine(lastPosition());
+        newPos -= _lastSuitPosition;
+        if (newPos == 0)
+            return;
+
+        if (gameObject.transform.position.y <= 0 && gameObject.transform.position.y != 3 && gameObject.transform.position.y != -4)
         {
             switch (suitsEnum)
             {
                 case SuitsEnum.Red:
-                   StartCoroutine(MoveDown(0.01f));
+                    StartCoroutine(MoveDown(0.01f));
                     _indexSuit = 1;
                     break;
 
@@ -33,27 +57,27 @@ public class IfSuitMove : MonoBehaviour
                     break;
             }
         }
-        if (gameObject.transform.position.y > 0)
+        if (gameObject.transform.position.y > 0 && gameObject.transform.position.y != 3 && gameObject.transform.position.y != -4)
         {
             switch (suitsEnum)
             {
                 case SuitsEnum.Red:
-                    StartCoroutine(MoveUp(0.01f));
+                    StartCoroutine(MoveUp(-0.01f));
                     _indexSuit = 1;
                     break;
 
                 case SuitsEnum.Blue:
-                    StartCoroutine(MoveUp(0.01f));
+                    StartCoroutine(MoveUp(-0.01f));
                     _indexSuit = 3;
                     break;
 
                 case SuitsEnum.Yellow:
-                    StartCoroutine(MoveUp(0.01f));
+                    StartCoroutine(MoveUp(-0.01f));
                     _indexSuit = 2;
                     break;
 
                 case SuitsEnum.Green:
-                    StartCoroutine(MoveUp(0.01f));
+                    StartCoroutine(MoveUp(-0.01f));
                     _indexSuit = 0;
                     break;
             }
@@ -63,15 +87,41 @@ public class IfSuitMove : MonoBehaviour
     private IEnumerator MoveDown(float newPosY)
     {
         yield return new WaitForSeconds(0.25f);
+        if (Suits[_indexSuit].transform.position.y >= 3)
+        {
+            Suits[_indexSuit].transform.position = new Vector3(Suits[_indexSuit].transform.position.x, 3, Suits[_indexSuit].transform.position.z);
+            yield return null;
+        }
+        if (Suits[_indexSuit].transform.position.y <= -4)
+        {
+            Suits[_indexSuit].transform.position = new Vector3(Suits[_indexSuit].transform.position.x, -4, Suits[_indexSuit].transform.position.z);
+            yield return null;
+        }
         Vector3 suitPosition = Suits[_indexSuit].transform.position;
-        suitPosition.y -= newPosY;
-        Suits[_indexSuit].transform.position = suitPosition;       
+        suitPosition.y -= newPosY - 0.1f;
+        Suits[_indexSuit].transform.position = suitPosition;
     }
     private IEnumerator MoveUp(float newPosY)
     {
+
         yield return new WaitForSeconds(0.25f);
+        if (Suits[_indexSuit].transform.position.y >= 3)
+        {
+            Suits[_indexSuit].transform.position = new Vector3(Suits[_indexSuit].transform.position.x, 3, Suits[_indexSuit].transform.position.z);
+            yield return null;
+        }
+        if (Suits[_indexSuit].transform.position.y <= -4)
+        {
+            Suits[_indexSuit].transform.position = new Vector3(Suits[_indexSuit].transform.position.x, -4, Suits[_indexSuit].transform.position.z);
+            yield return null;
+        }
         Vector3 suitPosition = Suits[_indexSuit].transform.position;
-        suitPosition.y -= newPosY;
-        Suits[_indexSuit].transform.position = suitPosition;        
+        suitPosition.y -= newPosY + 0.1f;
+        Suits[_indexSuit].transform.position = suitPosition;
+    }
+    private IEnumerator lastPosition()
+    {
+        _lastSuitPosition = gameObject.transform.position.y;
+        yield return null;
     }
 }
