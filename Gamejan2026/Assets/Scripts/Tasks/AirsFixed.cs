5deab7723c8afee523ffbcee24ada31c3a4ccdd5
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2026-08-20 09:30:56 -0300

    TeleportBarrier

--- Gamejan2026/Assets/Scripts/Tasks/AirsFixed.cs
+++ Gamejan2026/Assets/Scripts/Tasks/AirsFixed.cs
@@ -1,12 +1,16 @@
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class AirsFixed : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI airsTask;
-   
+    [SerializeField] private UnityEvent taskCompleted;
+    int airsFixed = 0;
   public void TaskAtualization(int airDone)
     {
+        airsFixed += airDone;
        airsTask.text = ("Ares-condicionados consertados: " + airDone + "/ 3" );
+        if(airsFixed == 3)
+            taskCompleted.Invoke();
     }
 }
