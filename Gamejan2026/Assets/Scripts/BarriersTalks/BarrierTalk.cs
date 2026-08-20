f181a864e2bfee85f08f6f3f779f278db25a4a55
Bernardorocha0710 <167346087+Bernardorocha0710@users.noreply.github.com>
2026-08-20 10:31:22 -0300

    Revert "Merge branch 'main' of https://github.com/YuriScheunemann/Gamejan2026"
    This reverts commit 2697a21f4e939c3e72d7a2fa829e6176349f7108, reversing
    changes made to 3661c45ce3260729dd814d7e8b3d7f0d827cbd8e.

--- Gamejan2026/Assets/Scripts/BarriersTalks/BarrierTalk.cs
+++ Gamejan2026/Assets/Scripts/BarriersTalks/BarrierTalk.cs
@@ -5,7 +5,21 @@ public class BarrierTalk : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+            DialogueActivation.Instance.StartDialogo();        
+=======
             DialogueActivation.Instance.StartDialogo();
-       
+        
+>>>>>>> Stashed changes
+=======
+            DialogueActivation.Instance.StartDialogo();
+        
+>>>>>>> Stashed changes
+=======
+            DialogueActivation.Instance.StartDialogo();
+        
+>>>>>>> Stashed changes
     }
 }
